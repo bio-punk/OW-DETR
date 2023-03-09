cb91b9ccce663bdd4b4f9a4732c005894dd48bca
WZK <cory@hnu.edu.cn>
2023-03-09 14:56:35 +0800

    Update singlenode-run_slurm.sh

--- singlenode-run_slurm.sh
+++ singlenode-run_slurm.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #
 #sBATCH --job-name=owdetr
-#SBATCH --gpus=16
-#SBATCH --nodes=2
-#SBATCH --time=3-00:00:00
+#SBATCH --gpus=8
+#SBATCH --nodes=1
+#SBATCH -p gpu_c128
 #
+# 1节点8卡gpu_c128队列运行
 
-GPUS_PER_NODE=8 ./tools/run_dist_slurm.sh berz deformable_detr 16 configs/OWOD_new_split.sh
+module load anaconda/2022.10 gcc/5.4 cuda/11.1
+
+source activate owdetr
+
+GPUS_PER_NODE=8 ./tools/run_dist_slurm.sh berz deformable_detr 8 configs/OWOD_20230309_split.sh
